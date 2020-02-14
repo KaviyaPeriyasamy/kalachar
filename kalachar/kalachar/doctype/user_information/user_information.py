24d3f7b1ce0231ac5bf9550e8c0ab41ed7f0fa10
Vignesh <kaviya.3111999@gmail.com>
2020-02-14 22:06:38 +0530

    getting dancer details

--- kalachar/kalachar/doctype/user_information/user_information.py
+++ kalachar/kalachar/doctype/user_information/user_information.py
@@ -17,3 +17,23 @@ def authenticate_user(phone_no,password):
 	else:
 		s.append("not allowed")
 	return s
+@frappe.whitelist()
+def get_dancer_detail(type_,purpose):
+	category={}
+	dancers=[]
+	dancer_list=frappe.get_all("User Information",filters={'dance_type':type_,'purpose':purpose})
+	for dancer in dancer_list:
+		dancer_doc=frappe.get_doc("User Information",dancer.name)
+		category['org_name']=dancer_doc.organization_name
+		category['org_addr']=dancer_doc.organization_address
+		category['org_phone']=dancer_doc.organization_phone_number
+		for charges in dancer_doc.charging_details:
+			if charges.purpose==purpose and charges.dance_type==type_:
+				category['timing']=charges.timing
+				category['advance_amount']=charges.advance_amount
+				category['full_amount']=charges.full_amount
+		dancers.append(category)
+	print(dancers)
+	return dancers
+
+
